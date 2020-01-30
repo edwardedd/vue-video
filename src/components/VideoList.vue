2ea1e8cc0facd0643eea54b58a3feac5c5e9234b
Eduard <eddmos86@gmail.com>
2020-01-30 18:13:27 +0300

    first commit

---
+++ src/components/VideoList.vue
@@ -0,0 +1,26 @@
+<template>
+  <ul class="list-group">
+    <VideoListItem
+     v-for="video in videos"
+      :video="video"
+      :key="video.etag">
+      <!-- {{video}} -->
+    </VideoListItem>
+  </ul>
+</template>
+
+<script>
+import VideoListItem from './VideoListItem';
+
+export default {
+  name: 'VideoList',
+  components: {
+    VideoListItem
+  },
+  props: ['videos']
+};
+</script>
+
+<style >
+
+</style>
